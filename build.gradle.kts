21d3590f1879bd02ce4e0a7c6cf07c15ac264b3b
Eduardo Granetto <eduardogranetto@gmail.com>
2022-09-15 14:45:13 -0300

    Enable actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,12 +24,12 @@ val grpcVersion      = "1.45.0"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.postgresql:postgresql")
-
 	implementation("org.springdoc:springdoc-openapi-ui:${openApiUiVersion}")
 
 	implementation("io.grpc:grpc-netty:${grpcVersion}")
